2a38fe95ad969720c53028dc2ac4da1404f5720c
FFiillaattoovv <admirator2015@yandex.ru>
2021-05-14 15:20:55 +0300

    Creates a Skill component and adds styling

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Skills.module.css';
 import StyleContainer from '../common/styles/Container.module.css';
+import Skill from './skill/Skill';
 
 function Skills() {
     return (
@@ -8,7 +9,14 @@ function Skills() {
             <div className={`${StyleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Мои скиллы</h2>
                 <div className={style.skills}>
-                    <h3>Skill</h3>
+                    <Skill title={'HTML/CSS'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                    <Skill title={'JavaScript'}
+                           description={'Excepturi inventore ipsa itaque.'}/>
+                    <Skill title={'TypeScript'}
+                           description={' Accusamus alias aspernatur corporis omnis pariatur reprehenderit, voluptates!'}/>
+                    <Skill title={'React/Redux'}
+                           description={'Minus molestias placeat quidem quos recusandae rerum sit voluptatem voluptatum!'}/>
                 </div>
             </div>
         </div>
